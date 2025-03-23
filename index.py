8d1290670a185bee4448252cc6000911b914a84c
VEER DODIYA <dodiyaveer12@gmail.com>
2025-03-23 21:16:46 +0530

    Change in UI

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ ctk.set_appearance_mode("system")
 
 if __name__ == "__main__":
     app = ctk.CTk()
+# Main Frame  
     main = ctk.CTkFrame(master=app,width=195,height=280)
     main.pack(expand=True, side="left", fill="both",padx = 5,pady = 5) 
     content_frame = ctk.CTkFrame(master=app,width=395,height=330)
@@ -12,15 +13,17 @@ if __name__ == "__main__":
 # For Adding The Entery 
     add_f = ctk.CTkFrame(master=content_frame)
     # add_f.pack(expand = False,side = "right",fill = "both",padx = 5,pady=5)
-    in_text = ctk.CTkEntry(master=add_f,width = 280,height= 30,placeholder_text="ENTER NOTE")
-    in_text.grid(row=0, column=0, padx=1, pady=1)
-    re_text = ctk.CTkEntry(master=add_f,width= 80,height=30,placeholder_text="AMOUNT")
-    re_text.grid(row = 0,column = 1,padx =3)
+    entery = ctk.CTkEntry(add_f,placeholder_text="Enter The Entery",width=70,height=30)
+    entery.place(x = 100,y = 200,)
+    
 # For removing The Entery
     remove_F = ctk.CTkFrame(master=content_frame)
+    
+
 
 # For Viwe All Entery
     book = ctk.CTkScrollableFrame(master=content_frame,)
+
 
 
 
@@ -30,7 +33,16 @@ if __name__ == "__main__":
 
     def show_frame(frame):
         frame.lift()   
-    
+    ctk.CTkButton(main,width=106,height=30,text="Add Entery",command=lambda:show_frame(add_f)).pack(padx = 10,pady = 10,side = "bottom")
+    ctk.CTkButton(main,width=30,height=30,text="REMOVE Entery",command=lambda:show_frame(remove_F)).pack(padx = 10,pady = 10,side = "bottom")
+    ctk.CTkButton(main,width=106,height=30,text="Show Entery",command=lambda:show_frame(book)).pack(pady = 10,side = "bottom")
+    mess = ctk.CTkLabel(master=main,text="Welcome To Money Management \n App",width=185,height=185)
+    mess.pack(side = "top",padx = 10,pady = 5)
+
+
+
+
+
 
     show_frame(add_f)
 
